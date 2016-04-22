c6120d6be125f9e43efd5e19f4d0430dabdfd297
ashutoshbondre <ashutoshbondre@gmail.com>
2016-04-22 15:10:00 +0530

    Added files via upload

---
+++ basic form.js
@@ -0,0 +1,9 @@
+function check()
+{
+  var e1=document.getElementById("email1");
+  var e2=document.getElementById("email2");
+  if( e1.value != e2.value)
+    {
+      alert("The two IDs dont match");
+    }
+}
