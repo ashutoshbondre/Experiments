3ac72044f3592d1267bd23046853325e98854021
ashutoshbondre <ashutoshbondre@gmail.com>
2016-04-25 15:10:28 +0530

    Added files via upload

---
+++ Shuffle.java
@@ -0,0 +1,30 @@
+public class Shuffle {
+	
+	
+	public static void shuffle(Object[] a)
+	{
+		int N = a.length;
+		for (int i =0; i <N ;i ++)
+		{
+			int r =StdRandom.uniform(i+1);
+			//System.out.print( r +" #");
+			exch(a,i,r);
+		}
+		
+	}
+	public static void exch (Object a[],int i,int j)
+	{
+		Object temp = a[i];
+		a[i]=a[j];
+		a[j]=temp;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer arr[]= {1,2,3,4,5,6,7,8,0};
+		shuffle(arr);
+		for(int i = 0;i<arr.length;i++)
+		System.out.print(arr[i]+ " ");
+	}
+
+}
