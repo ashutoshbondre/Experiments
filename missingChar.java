df0bc12ec30b0ba1bb376489cd79c4a3ab5ece07
ashutoshbondre <ashutoshbondre@gmail.com>
2016-06-05 14:05:28 +0530

    Add files via upload

---
+++ missingChar.java
@@ -0,0 +1,32 @@
+public String missingChar(String str, int n) 
+{
+  StringBuilder sb= new StringBuilder();
+ 
+ char [] c = str.toCharArray();
+  
+int length = c.length;
+  
+if(n!=0)
+ 
+ {
+    
+	for(int i =0;i<n;i++)
+  
+	{
+    sb.append(c[i]);
+  
+	}
+ 
+ }
+  
+
+for(int i=n+1;i<=length-1;i++)
+  
+{
+    sb.append(c[i]);
+  }
+ 
+ return sb.toString();
+
+}
+
